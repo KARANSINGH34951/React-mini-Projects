b10ca7df531f772eccae5d60641a6a79d0776c67
karan singh D <ceitkaransingh2026@gmail.com>
2024-04-29 19:07:34 +0530

    added menu and api related code

--- Hotel website/src/components/Restaurantmenu.jsx
+++ Hotel website/src/components/Restaurantmenu.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Shimmercards from './Shimmercards'
+import {useState} from 'react'
 
 const Restaurantmenu = () => {
+  const [resinfo,setresinfo]=useState(null)
+
+  useEffect(()=>{
+      fetchmenu()
+  },[])
+
+  const fetchmenu = async () => { 
+    const data=await fetch('https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.3581441&lng=77.7135612&restaurantId=220269&catalog_qa=undefined&isMenuUx4=true&submitAction=ENTER')
+    const json =await data.json()
+    console.log(json);
+  }
+  // https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=
+  if(resinfo===null){
+    <Shimmercards/>
+  }
+
   return (
     <div className='menu'>
       <h1>Name of res</h1>
--- Hotel website/src/main.jsx
+++ Hotel website/src/main.jsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
         element:<Contact/>
       },
       {
-        path:'/restaurants/:id',
+        path:'/restaurant/:id',
         element:<Restaurantmenu/>
       
       }
